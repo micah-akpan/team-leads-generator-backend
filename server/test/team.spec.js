77554d5df60230a894744154d636ebecdd619a13
Micah Akpan <mickey4sure2k7@gmail.com>
2019-03-21 18:29:43 +0100

    Add new tests and implementation

--- server/test/team.spec.js
+++ server/test/team.spec.js
@@ -12,5 +12,24 @@ describe('Team API', () => {
             agent.get('/api/users')
               .expect(200, done);
         })
+
+        it('should return a single team member', (done) => {
+            agent.get('/api/users/1')
+              .expect(200, done);
+        })
+    })
+
+    describe('Update team members', () => {
+        it('should update the status of a team member', (done) => {
+            agent.patch('/api/users/1')
+              .send({
+                  served: true,
+              })
+              .expect(200)
+              .end((err, res) => {
+                  res.body.data[0].served.should.equal(true);
+                  done();
+              })
+        })
     })
 })
